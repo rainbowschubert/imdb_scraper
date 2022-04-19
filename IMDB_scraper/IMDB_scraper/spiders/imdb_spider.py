bafaf8e1671f7b05263a8c5b12ed54c94ce4d04a
gerubach <ashih91@gmail.com>
2022-04-18 19:03:41 -0700

    modifying settings and creating spider

---
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -0,0 +1,9 @@
+# to run 
+# scrapy crawl imdb_spider -o movies.csv
+
+import scrapy
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb_spider'
+    
+    start_urls = ['https://www.imdb.com/title/tt1641384/?ref_=nv_sr_srsg_0']
