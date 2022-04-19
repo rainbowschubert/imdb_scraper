7060c6f32a9fd232414b9cb02943457b4641019d
gerubach <ashih91@gmail.com>
2022-04-18 20:04:13 -0700

    added parse methods

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -7,3 +7,9 @@ class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
     
     start_urls = ['https://www.imdb.com/title/tt1641384/?ref_=nv_sr_srsg_0']
+
+    def parse(self, response):
+        yield scrapy.Request("https://www.imdb.com/title/tt1641384/fullcredits/?ref_=tt_ql_cl", callback=parse_full_credits(self,response))
+    
+    def parse_full_credits(self, response):
+        yield [a.attrib["href"] for a in response.css("td.primary_photo a")]
