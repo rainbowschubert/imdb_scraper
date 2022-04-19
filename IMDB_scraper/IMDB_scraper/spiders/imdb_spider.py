0a6c1e09ad7070aaef53e89c68f15c115bbc3158
gerubach <ashih91@gmail.com>
2022-04-19 07:50:34 -0700

    getting spider working

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -9,7 +9,17 @@ class ImdbSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/title/tt1641384/?ref_=nv_sr_srsg_0']
 
     def parse(self, response):
-        yield scrapy.Request("https://www.imdb.com/title/tt1641384/fullcredits/?ref_=tt_ql_cl", callback=parse_full_credits(self,response))
+        yield scrapy.Request("https://www.imdb.com/title/tt1641384/fullcredits/?ref_=tt_ql_cl", self.parse_full_credits)
     
     def parse_full_credits(self, response):
-        yield [a.attrib["href"] for a in response.css("td.primary_photo a")]
+        for link in [a.attrib["href"] for a in response.css("td.primary_photo a")]:
+            yield scrapy.Request(response.urljoin(link), callback=self.parse_actor_page)
+    
+    def parse_actor_page(self, response):
+        name = response.css("title::text").get()[:-7]
+        for item in response.css("div.filmo-row"):
+            show_name = item.css("b a::text").get()
+            yield {
+                "actor" : name,
+                "movie_or_TV_name" : show_name
+            }
